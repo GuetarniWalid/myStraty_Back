bdc2f7cf0073b76a10e007eac85d3bfc27652a43
walid guetarni <guetarni.walid@gmail.com>
2021-03-18 14:58:04 +0100

    correction error in migration fro chat, remove rule "pseudo unique"

--- app/Controllers/Ws/ChatController.js
+++ app/Controllers/Ws/ChatController.js
@@ -27,7 +27,6 @@ class ChatController {
    * @returns {void}
    */
   onMessage(message) {
-    console.log("🚀 ~ file: ChatController.js ~ line 30 ~ ChatController ~ onMessage ~ message", message)
     //if text is empty, no continuation
     if(!message.message.length) return
     const formatMessage = {
